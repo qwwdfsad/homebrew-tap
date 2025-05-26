84f02e70adc81f997655bf5e8f505165592f434c
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2025-05-26 18:04:56 +0200

    Amper 0.7.0

--- amper.rb
+++ amper.rb
@@ -2,16 +2,16 @@ class Amper < Formula
   desc "A project configuration and build tool with a focus on the user experience and the IDE support"
   homepage "https://github.com/JetBrains/amper/"
   license "Apache-2.0"
-  url "https://packages.jetbrains.team/maven/p/amper/amper/org/jetbrains/amper/cli/0.6.0/cli-0.6.0-wrapper?download=true"
-  sha256 "cfddb5d2e4d00a3464e5457ddbb60414184eef423da24fe236d4264e68a7b5f0"
+  url "https://packages.jetbrains.team/maven/p/amper/amper/org/jetbrains/amper/cli/0.7.0/cli-0.7.0-wrapper?download=true"
+  sha256 "0788b201e2b62555c3024775f6ae9046712ff8d9a07aae3fe705277e9691fe6f"
   version "0.6.0"
 
   depends_on "kdoctor"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/cli-0.6.0-wrapper" => "amper"
-    chmod "+x", "#{libexec}/cli-0.6.0-wrapper"
+    bin.install_symlink "#{libexec}/cli-0.7.0-wrapper" => "amper"
+    chmod "+x", "#{libexec}/cli-0.7.0-wrapper"
   end
 
   def caveats
